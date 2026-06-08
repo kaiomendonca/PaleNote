707b339641aae8e07778a1c227a2d75c3dbec8b3
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-06-07 22:36:35 -0300

    refactor: remove hardcoded in variables

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    LOG_LEVEL: str = "INFO"
-    ENVIRONMENT: str = "development"
+    LOG_LEVEL: str
+    ENVIRONMENT: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
