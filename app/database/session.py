3f7078fb28a64cc54d479fac65e65130eb4954e7
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-06-20 12:56:26 -0300

    refactor(session): migrate session to async session

--- app/database/session.py
+++ app/database/session.py
@@ -1,11 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.config import settings
 
-engine = create_engine(
+engine = create_async_engine(
     settings.DATABASE_URL,
     echo=False,
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
