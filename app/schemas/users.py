e7662000961a00fdfd93f96e9afff08779e9e478
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-07-02 15:42:29 -0300

    feat: Created userresponse, userupdate and changepassword schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr, Field
+
+from app.models.users import PersonType
 
 
 class UserCreate(BaseModel):
@@ -21,3 +26,27 @@ class UserCreate(BaseModel):
         max_length=14,
         description="Individual taxpayer ID (CPF) or corporate taxpayer ID (CNPJ)",
     )
+
+
+class UserResponse(BaseModel):
+    name: str
+
+    email: EmailStr
+
+    person_type: PersonType
+
+    document: str
+
+    created_at: datetime
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = Field(None, min_length=3, max_length=200)
+    email: Optional[EmailStr] = None
+    person_type: Optional[PersonType] = None
+    document: Optional[str] = Field(None, min_length=11, max_length=14)
+
+
+class ChangePassword(BaseModel):
+    current_password: str = Field(..., min_length=8, max_length=128)
+    new_password: str = Field(..., min_length=8, max_length=128)
