beb9e2279b18d1dcc5f39ea6c593d482dc128c7b
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-07-16 12:39:49 -0300

    Created user_repository for database communication

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,32 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.users import Users
+
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create_user(self, user_data: Users) -> Users:
+        self.db.add(user_data)
+        await self.db.flush()
+        return user_data
+
+    async def get_by_document(self, document: str) -> Users | None:
+        query = select(Users).where(Users.document == document)
+        result = await self.db.execute(query)
+        user = result.scalar_one_or_none()
+        return user
+
+    async def get_by_email(self, email: str) -> Users:
+        query = select(Users).where(Users.email == email)
+        result = await self.db.execute(query)
+        user_by_email = result.scalar_one_or_none()
+        return user_by_email
+
+    async def update_password(self, user_id: str, password: str) -> None:
+        query = select(Users).where(Users.id_ == user_id)
+        result = await self.db.execute(query)
+        user = result.scalar_one_or_none()
+        user.password_hash = password
